cdd31f0c22f8b9686e43c49f850eeea0701317ae
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-27 08:07:24 -0600

    Refactor configOkClient method to single line

---
+++ app/src/legacy21/java/org/CreadoresProgram/RetroCreaBrowser/browserconfig/SetConfigOkClient.java
@@ -0,0 +1,16 @@
+package org.CreadoresProgram.RetroCreaBrowser.browserconfig;
+
+
+import okhttp3.TlsVersion;
+import okhttp3.ConnectionSpec;
+
+
+import java.util.Arrays;
+
+
+import org.CreadoresProgram.WebViewCREA.network.NetClient;
+
+
+public class SetConfigOkClient {
+  public static void configOkClient(NetClient client){}
+}
