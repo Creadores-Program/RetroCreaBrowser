1e896782141b8fb98ae2b2352779b096d235f36a
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-27 07:58:29 -0600

    Add RetroCreaBrowserApp class for application initialization

---
+++ app/src/legacy21/java/org/CreadoresProgram/RetroCreaBrowser/RetroCreaBrowserApp.java
@@ -0,0 +1,10 @@
+package org.CreadoresProgram.RetroCreaBrowser;
+
+import android.app.Application;
+
+public class RetroCreaBrowserApp extends Application {
+    @Override
+    public void onCreate(){
+        super.onCreate();
+    }
+}
