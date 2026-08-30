9889aff8ba3c2833402b8ae5a2719e8e0ad49547
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-30 09:44:40 -0600

    Modify configOkClient to accept Context and setup cache
    Added context parameter to configOkClient method and implemented cache directory setup.

--- app/src/standard/java/org/CreadoresProgram/RetroCreaBrowser/browserconfig/SetConfigOkClient.java
+++ app/src/standard/java/org/CreadoresProgram/RetroCreaBrowser/browserconfig/SetConfigOkClient.java
@@ -1,14 +1,19 @@
 package org.CreadoresProgram.RetroCreaBrowser.browserconfig;
 
+import android.content.Context;
+
 import okhttp3.TlsVersion;
 import okhttp3.ConnectionSpec;
+import okhttp3.Cache;
 
 import java.util.Arrays;
+import java.io.File;
 
 import org.CreadoresProgram.WebViewCREA.network.NetClient;
 
 public class SetConfigOkClient {
-  public static void configOkClient(NetClient client){
+  public static void configOkClient(NetClient client, Context context){
+      File cacheDir = new File(context.getCacheDir(), "patch_cache");
       client.setOkClient(client.getOkClient().newBuilder()
           .connectionSpecs(Arrays.asList(
               new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
@@ -19,6 +24,7 @@ public class SetConfigOkClient {
                   .supportsTlsExtensions(true)
                   .build()
           ))
+          .cache(new Cache(cacheDir, 30 * 1024 * 1024))
           .build());
   }
 }
