0df7d83ed6bcdf5761999ee66023be7509b7a42f
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-30 09:45:09 -0600

    Add Context parameter to configOkClient method

--- app/src/legacy21/java/org/CreadoresProgram/RetroCreaBrowser/browserconfig/SetConfigOkClient.java
+++ app/src/legacy21/java/org/CreadoresProgram/RetroCreaBrowser/browserconfig/SetConfigOkClient.java
@@ -1,7 +1,8 @@
 package org.CreadoresProgram.RetroCreaBrowser.browserconfig;
 
+import android.content.Context;
 import org.CreadoresProgram.WebViewCREA.network.NetClient;
 
 public class SetConfigOkClient {
-  public static void configOkClient(NetClient client){}
+  public static void configOkClient(NetClient client, Context, context){}
 }
