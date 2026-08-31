9c4f104282262671122811d834d70af28f6c3fd6
Trollhunters501 <trollhunters500@outlook.com>
2026-08-30 21:46:31 -0600

    Fix imports

--- app/src/main/java/org/CreadoresProgram/RetroCreaBrowser/MainActivity.java
+++ app/src/main/java/org/CreadoresProgram/RetroCreaBrowser/MainActivity.java
@@ -8,7 +8,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Build;
