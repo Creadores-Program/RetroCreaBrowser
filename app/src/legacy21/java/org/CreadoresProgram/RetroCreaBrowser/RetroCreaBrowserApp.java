c27c45220b0a11ab3d26106e85608778b96b4b26
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-09-03 13:23:30 -0600

    Implement tab management in RetroCreaBrowserApp
    Added functionality to manage tabs in RetroCreaBrowserApp.

--- app/src/legacy21/java/org/CreadoresProgram/RetroCreaBrowser/RetroCreaBrowserApp.java
+++ app/src/legacy21/java/org/CreadoresProgram/RetroCreaBrowser/RetroCreaBrowserApp.java
@@ -2,4 +2,23 @@ package org.CreadoresProgram.RetroCreaBrowser;
 
 import android.app.Application;
 
-public class RetroCreaBrowserApp extends Application {}
+import java.util.List;
+import java.util.ArrayList;
+
+import org.CreadoresProgram.RetroCreaBrowser.MainActivity;
+
+public class RetroCreaBrowserApp extends Application {
+    private final List<MainActivity> tabs = new ArrayList<MainActivity>();
+
+    public void addTab(MainActivity tab){
+        tabs.add(tab);
+    }
+
+    public void removeTab(MainActivity tab){
+        tabs.remove(tab);
+    }
+
+    public List<MainActivity> getTabs(){
+        return new ArrayList<MainActivity>(tabs);
+    }
+}
