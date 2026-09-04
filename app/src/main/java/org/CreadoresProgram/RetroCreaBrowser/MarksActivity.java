4fad3ac99a8b1099d36410866e398291e2042a1d
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-09-03 22:19:48 -0600

    Add import for Context in MarksActivity

--- app/src/main/java/org/CreadoresProgram/RetroCreaBrowser/MarksActivity.java
+++ app/src/main/java/org/CreadoresProgram/RetroCreaBrowser/MarksActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Build;
 import android.content.Intent;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +15,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.ImageView;
 import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.List;
 
