859d3f6999483168084ec418207acffdedb44534
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-09-07 22:05:59 -0600

    Fix newline character in JavaScript evaluation

--- app/src/main/java/org/CreadoresProgram/RetroCreaBrowser/MainActivity.java
+++ app/src/main/java/org/CreadoresProgram/RetroCreaBrowser/MainActivity.java
@@ -527,7 +527,7 @@ public class MainActivity extends Activity {
         viewSourceBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                WebViewUtils.evaluateJS(webView, "(function (){var h=document.documentElement.outerHTML||document.createElement('div').appendChild(document.documentElement.cloneNode(true)).parentNode.innerHTML;document.open('text/plain','replace');document.write('<!DOCTYPE html>\n'+h);document.close();})();");
+                WebViewUtils.evaluateJS(webView, "(function (){var h=document.documentElement.outerHTML||document.createElement('div').appendChild(document.documentElement.cloneNode(true)).parentNode.innerHTML;document.open('text/plain','replace');document.write('<!DOCTYPE html>\\n'+h);document.close();})();");
             }
         });
 
