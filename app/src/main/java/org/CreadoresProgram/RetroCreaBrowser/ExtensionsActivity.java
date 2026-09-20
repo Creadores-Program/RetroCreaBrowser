c62e3ff7a85c5b5de838e46d7b6926e5ace93943
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-09-19 22:58:10 -0600

    Fix intent context in ExtensionsActivity

--- app/src/main/java/org/CreadoresProgram/RetroCreaBrowser/ExtensionsActivity.java
+++ app/src/main/java/org/CreadoresProgram/RetroCreaBrowser/ExtensionsActivity.java
@@ -34,7 +34,7 @@ public class ExtensionsActivity extends Activity {
         viewExts.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MarksActivity.this, MainActivity.class);
+                Intent intent = new Intent(ExtensionsActivity.this, MainActivity.class);
                 //intent.setData(Uri.parse("https://urlextspage"));
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
                 startActivity(intent);
