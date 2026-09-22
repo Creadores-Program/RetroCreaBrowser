8ff4a675731417e7ffc26eb430135a7091656d0f
Trollhunters501 <trollhunters500@outlook.com>
2026-09-21 22:09:58 -0600

    Fix Exts

---
+++ app/src/main/java/org/CreadoresProgram/RetroCreaBrowser/ExtensionManager.java
@@ -0,0 +1,95 @@
+package org.CreadoresProgram.RetroCreaBrowser;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.text.TextUtils;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExtensionManager {
+    private static final String PREF_NAME = "extensions_pref";
+    private static final String KEY_EXTENSIONS = "extensions_list";
+
+    public static class Extension {
+        public String name;
+        public String scriptCode;
+        public boolean enabled;
+
+        public Extension(String name, String scriptCode, boolean enabled) {
+            this.name = name;
+            this.scriptCode = scriptCode;
+            this.enabled = enabled;
+        }
+    }
+
+    public static List<Extension> getExtensions(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        String rawData = prefs.getString(KEY_EXTENSIONS, null);
+        List<Extension> list = new ArrayList<Extension>();
+
+        if (rawData == null || TextUtils.isEmpty(rawData)) {
+            list.add(new Extension("Force Dark", "document.body.style.backgroundColor='#121212'; document.body.style.color='#FFFFFF';", false));
+            list.add(new Extension("Block Popups", "window.open = function(){ return null; };", false));
+            saveExtensions(context, list);
+        } else {
+            String[] items = rawData.split("###");
+            for (String item : items) {
+                String[] parts = item.split(":::");
+                if (parts.length == 3) {
+                    boolean isEnabled = Boolean.parseBoolean(parts[2]);
+                    list.add(new Extension(parts[0], parts[1], isEnabled));
+                }
+            }
+        }
+        return list;
+    }
+
+    public static void saveExtensions(Context context, List<Extension> list) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            Extension ext = list.get(i);
+            sb.append(ext.name).append(":::").append(ext.scriptCode).append(":::").append(ext.enabled);
+            if (i < list.size() - 1) {
+                sb.append("###");
+            }
+        }
+
+        SharedPreferences.Editor editor = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE).edit();
+        editor.putString(KEY_EXTENSIONS, sb.toString());
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+            editor.apply();
+        } else {
+            editor.commit();
+        }
+    }
+
+    public static void addExtension(Context context, String name, String scriptCode) {
+        List<Extension> list = getExtensions(context);
+        list.add(new Extension(name, scriptCode, true));
+        saveExtensions(context, list);
+    }
+
+    public static void removeExtension(Context context, int index) {
+        List<Extension> list = getExtensions(context);
+        if (index >= 0 && index < list.size()) {
+            list.remove(index);
+            saveExtensions(context, list);
+        }
+    }
+    public static void toggleExtension(Context context, int index, boolean enabled) {
+        List<Extension> list = getExtensions(context);
+        if (index >= 0 && index < list.size()) {
+            list.get(index).enabled = enabled;
+            saveExtensions(context, list);
+        }
+    }
+    public static void removeExtension(Context context, int index) {
+        List<Extension> list = getExtensions(context);
+        if (index >= 0 && index < list.size()) {
+            list.remove(index);
+            saveExtensions(context, list);
+        }
+    }
+}
--- app/src/main/java/org/CreadoresProgram/RetroCreaBrowser/ExtensionsActivity.java
+++ app/src/main/java/org/CreadoresProgram/RetroCreaBrowser/ExtensionsActivity.java
@@ -1,46 +1,147 @@
 package org.CreadoresProgram.RetroCreaBrowser;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.Window;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.ListView;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class ExtensionsActivity extends Activity {
 
     private ListView listView;
-    
+    private ExtensionAdapter adapter;
+    private List<ExtensionManager.Extension> extensionsList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        if(Build.VERSION.SDK_INT <= Build.VERSION_CODES.GINGERBREAD){
+        
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.GINGERBREAD) {
             requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         }
+        
         setContentView(R.layout.layout_config);
-        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.GINGERBREAD){
+        
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.GINGERBREAD) {
             getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.top_bar);
             TextView actionBarTitle = (TextView) findViewById(R.id.top_bar_title);
             actionBarTitle.setText(R.string.exts);
         }
+
         this.listView = (ListView) findViewById(R.id.listViewExts);
         TextView emptyView = (TextView) findViewById(R.id.emptyViewExts);
         this.listView.setEmptyView(emptyView);
+
+        extensionsList = ExtensionManager.getExtensions(this);
+        adapter = new ExtensionAdapter(this, extensionsList);
+        this.listView.setAdapter(adapter);
+
+        this.listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                final ExtensionManager.Extension ext = extensionsList.get(position);
+                ExtensionManager.removeExtension(ExtensionsActivity.this, position);
+                extensionsList.remove(position);
+                adapter.notifyDataSetChanged();
+                Toast.makeText(ExtensionsActivity.this, R.string.deleted_ext, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
         Button viewExts = (Button) findViewById(R.id.btnSearchExts);
         viewExts.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ExtensionsActivity.this, MainActivity.class);
-                //intent.setData(Uri.parse("https://urlextspage"));
+                //intent.setData(Uri.parse(URL));
+                intent.setAction(Intent.ACTION_VIEW);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
                 startActivity(intent);
                 finish();
             }
         });
+    }
+
+    private class ExtensionAdapter extends BaseAdapter {
+        private final Context context;
+        private final List<ExtensionManager.Extension> list;
+        private final LayoutInflater inflater;
+
+        public ExtensionAdapter(Context context, List<ExtensionManager.Extension> list) {
+            this.context = context;
+            this.list = list;
+            this.inflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        private class ViewHolder {
+            TextView txtName;
+            TextView txtCode;
+            CheckBox checkEnabled;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.item_ext, parent, false);
+                holder = new ViewHolder();
+                holder.txtName = (TextView) convertView.findViewById(R.id.textExtName);
+                holder.txtCode = (TextView) convertView.findViewById(R.id.textExtCode);
+                holder.checkEnabled = (CheckBox) convertView.findViewById(R.id.checkExtEnabled);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            final ExtensionManager.Extension ext = list.get(position);
+            holder.txtName.setText(ext.name);
+            holder.txtCode.setText(ext.scriptCode);
+
+            holder.checkEnabled.setOnCheckedChangeListener(null);
+            holder.checkEnabled.setChecked(ext.enabled);
+
+            holder.checkEnabled.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    ext.enabled = isChecked;
+                    ExtensionManager.toggleExtension(context, position, isChecked);
+                }
+            });
+
+            return convertView;
+        }
     }
 }
